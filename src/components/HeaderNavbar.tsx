8d35bebc9fc737d1917d8a0b07de9eb032fa7aef
佑 宋 <a47462543@gmail.com>
2026-06-01 01:29:35 +0800

    新增「漢堡選單按鈕」

--- src/components/HeaderNavbar.tsx
+++ src/components/HeaderNavbar.tsx
@@ -1,4 +1,5 @@
-import { TbTerminal2 } from 'react-icons/tb';
+import { useEffect, useRef, useState } from 'react';
+import { TbTerminal2, TbMenu2, TbX } from 'react-icons/tb';
 
 const titleList = [
   { id: 'about', nameEl: 'About', nameZh: '關於' },
@@ -7,17 +8,61 @@ const titleList = [
 ] as const;
 
 export default function HeaderNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <nav className="sticky top-0 z-50 bg-gray-950/80 backdrop-blur-sm border-b border-gray-800">
+    <nav
+      ref={navRef}
+      className="sticky top-0 z-50 bg-gray-950/80 backdrop-blur-sm border-b border-gray-800"
+    >
       <div className="max-w-7xl mx-auto px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-3">
+        <button
+          className="md:hidden text-2xl text-gray-300 p-1 border border-gray-800 rounded"
+          onClick={() => {
+            setIsOpen((val) => !val);
+          }}
+        >
+          {isOpen ? <TbX /> : <TbMenu2 />}
+        </button>
+
+        {/* 首頁Logo */}
         <div className="text-xl font-bold font-mono text-cyan-400">
-          <a href="#" className="flex gap-2 items-center">
+          <a
+            href="#"
+            className="flex gap-2 items-center"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
             <TbTerminal2 />
             <span>首頁</span>
           </a>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-6 text-sm text-gray-300">
+        {/* 導覽列內容列表 */}
+        <div
+          className={`
+            ${isOpen ? 'flex' : 'hidden'}
+            md:flex flex-col md:flex-row gap-6 text-sm text-gray-300
+          `}
+        >
           {titleList.map((item) => (
             <div
               key={item.id}
@@ -26,6 +71,9 @@ export default function HeaderNavbar() {
               <a
                 href={`#${item.id.toLocaleLowerCase()}`}
                 className=" group-hover:text-cyan-400 transition-colors duration-300"
+                onClick={() => {
+                  setIsOpen(false);
+                }}
               >
                 {item.nameZh}
               </a>
